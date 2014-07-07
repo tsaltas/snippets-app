985b94b70fc188953467ece54094a1b88991ea48
Shannon Tsaltas <shannontsaltas@gmail.com>
2014-07-06 21:13:15 -0500

    created argument parser

--- snippets.py
+++ snippets.py
@@ -1,8 +1,17 @@
+import sys
+import argparse
 import logging
 import csv
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
+
+def make_parser():
+    """ Construct the command line parser """
+    logging.info("Constructing parser")
+    description = "Store and retrieve snippets of text"
+    parser = argparse.ArgumentParser(description=description)
+    return parser
 
 def put(name, snippet, filename):
     """ Store a snippet with an associated name in the CSV file """
@@ -14,3 +23,12 @@ def put(name, snippet, filename):
         writer.writerow([name, snippet])
     logging.debug("Write sucessful")
     return name, snippet
+
+def main():
+    """ Main function """
+    logging.info("Starting snippets")
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
